01f3be12e2139ec2ec245f09eee83fad34295f0f
V1LE <v1lefarm.net@gmail.com>
2026-02-01 17:27:19 +1030

    Update charts.py

--- app/charts.py
+++ app/charts.py
@@ -1,1 +1,19 @@
+from collections import deque
 
+class Sparkline:
+    def __init__(self, size=30):
+        self.data = deque(maxlen=size)
+
+    def add(self, value):
+        self.data.append(value)
+
+    def render(self):
+        blocks = "▁▂▃▄▅▆▇█"
+        if not self.data:
+            return ""
+        mn, mx = min(self.data), max(self.data)
+        span = mx - mn or 1
+        return "".join(
+            blocks[int((v - mn) / span * (len(blocks) - 1))]
+            for v in self.data
+        )
